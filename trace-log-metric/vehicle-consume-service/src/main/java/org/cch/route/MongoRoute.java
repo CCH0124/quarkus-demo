56e95f7be91c3ab977fd98ce7bd23c4b4b7d918c
CCH0124 <cchong0124@gmail.com>
2023-02-16 20:58:48 +0800

    fix consumer mongo client

--- trace-log-metric/vehicle-consume-service/src/main/java/org/cch/route/MongoRoute.java
+++ trace-log-metric/vehicle-consume-service/src/main/java/org/cch/route/MongoRoute.java
@@ -5,6 +5,7 @@ import javax.inject.Inject;
 
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jackson.JacksonDataFormat;
 
 import com.mongodb.client.MongoClient;
 
@@ -12,22 +13,27 @@ import io.quarkus.mongodb.MongoClientName;
 
 @ApplicationScoped
 public class MongoRoute extends RouteBuilder {
-
-    @Inject
-    @MongoClientName("mongoVehicleClient")
-    MongoClient mongoClient1;
+    // multiple MongoDB servers
+    // @Inject
+    // @MongoClientName("vehicle")
+    // MongoClient mongoVehicleClient;
 
     @Override
     public void configure() throws Exception {
+
+
         from("direct:vihicleMongoInsert")
+                .routeId("VehicleToMongo")
                 .log(LoggingLevel.INFO, "MongoDbOperation: ${headers[CamelMongoDbOperation]}")
                 .log(LoggingLevel.INFO, "CamelMongoDbResultTotalSize: ${headers[CamelMongoDbResultTotalSize]}")
                 .log(LoggingLevel.INFO, "CamelMongoDbDatabase: ${headers[CamelMongoDbDatabase]}")
                 .log(LoggingLevel.INFO, "CamelMongoDbCollection: ${headers[CamelMongoDbCollection]}")
-                .to("mongodb:mongoVehicleClient?database=myOtherDb&collection=myCollection&operation=insert")
+                .threads(5).maxQueueSize(10)
+                .to("mongodb:camelMongoClient?database=vehicle&collection=vehicles&operation=save&createCollection=true")
                 .log(LoggingLevel.INFO, "CamelMongoWriteResult: ${headers[CamelMongoWriteResult]}")
                 .log(LoggingLevel.INFO, "_id: ${headers[_id]}");
                 
+
     }
 
 }
--- trace-log-metric/vehicle-consume-service/src/main/java/org/cch/stream/BrandEvent.java
+++ trace-log-metric/vehicle-consume-service/src/main/java/org/cch/stream/BrandEvent.java
@@ -5,8 +5,10 @@ import javax.inject.Inject;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.component.mongodb.MongoDbConstants;
 import org.apache.camel.component.mongodb.MongoDbOperation;
+import org.apache.camel.component.mongodb.converters.MongoDbBasicConverters;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.bson.Document;
 import org.cch.model.Brand;
-import org.cch.model.Vehicle;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.jboss.logging.Logger;
 import java.util.Map;
@@ -16,6 +18,7 @@ public class BrandEvent {
     @Inject
     Logger logger;
 
+    @Inject
     ProducerTemplate template;
 
 
@@ -27,8 +30,12 @@ public class BrandEvent {
 
 
     @Incoming("played-vehicles")
-    public void vehicleInsert(Vehicle vehicle) {
+    public void vehicleInsert(ConsumerRecord<String, String> record) {
+        logger.infov("Vehicle: {0}", record.value());
         Map<String, Object> scoreMap = Map.of(MongoDbConstants.COLLECTION, "vehicles", MongoDbConstants.DATABASE, "vehicle", MongoDbConstants.OPERATION_HEADER, MongoDbOperation.insert);
-        template.requestBodyAndHeaders("direct:vihicleMongoInsert", vehicle, scoreMap);
+        Document fromStringToDocument = MongoDbBasicConverters.fromStringToDocument(record.value());
+        logger.infov("Document: {0}", fromStringToDocument.toString());
+        template.sendBodyAndHeaders("direct:vihicleMongoInsert", fromStringToDocument, scoreMap);
+
     }
 }
